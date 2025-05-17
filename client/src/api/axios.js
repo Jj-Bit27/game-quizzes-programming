f3fdf4077b9f073e13bc53618bc1a958740ac88a
Juan Jose <22308051220637@cbtis122.edu.mx>
2025-05-17 12:44:18 -0600

    update file axios

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 /* La url del servidor */
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://game-quizzes-programming.onrender.com/api";
 
 /* Crear una instancia de axios */
 const instance = axios.create({
